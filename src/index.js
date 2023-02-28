6528d8a34493125471440d93aac108a1cc835916
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-02-28 16:02:09 -0700

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const submit = document.getElementById("submit-box");
+submit.addEventListener("click", submitClick, false);
+
+function submitClick(event) {
+  document.getElementById("tasks").innerHTML += "<li>" +  document.querySelector('input').value + "</li>";
+  event.preventDefault()
+}
